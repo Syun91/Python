54d4e7157624bdf2b0b3364e0815ca5d29e578e2
Syun91 <340020779@qq.com>
2020-12-03 18:33:42 +0800

    11111111

--- YW/app.py
+++ YW/app.py
@@ -1,9 +1,14 @@
 from flask import Flask,render_template,Response,make_response,Request
-
+from flask_sqlalchemy import SQLAlchemy
+import os
+import click
 app = Flask(__name__)
 
 
 app.config.from_pyfile("settings.py")
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///E:\Python_file\Python_Git\YW\YW_PY.db'
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
@@ -22,9 +27,59 @@ def index():
 ]
     return render_template('index.html', name=name, movies=movies)
 
+##创建表
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
 
 
 
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
+
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    
+    return render_template('404.html'), 404  # 返回模板和状态码
 
 if __name__ == '__main__':
    
